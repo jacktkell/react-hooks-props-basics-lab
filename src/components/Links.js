6507789097f6257bd44e43023a76384916e18ae4
jack <jack.kelling7@gmail.com>
2021-06-08 08:00:01 -0500

    commit tree 3ebf774dd5d14ebeb92c71a4432c43e28db41f44

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default Links
+
+function Links() {
+  return (
+    <div id="links">
+      <h3>Links</h3>
+      <a href="https://github.com/liza">https://github.com/liza</a>
+      <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
+    </div>
+  );
+}
